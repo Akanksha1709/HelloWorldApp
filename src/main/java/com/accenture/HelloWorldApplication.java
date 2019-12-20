d02e3f2045d94664ca1b678e95ec61e120ede8c1
Saxena <akanksha.e.saxena@accenture.com>
2019-12-20 11:30:13 +0530

    initial commit

---
+++ src/main/java/com/accenture/HelloWorldApplication.java
@@ -0,0 +1,21 @@
+package com.accenture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+
+	 @RequestMapping("/")
+    public String getEmployees() 
+    {
+      
+      return "Hello World!"
+    }
+
+}
